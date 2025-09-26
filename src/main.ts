ddf74ed597df4dbe56ee78621b8e11ffcfe3491d
pcamocardi <pablocamocardi@gmail.com>
2025-09-26 13:56:05 +0200

    feat: Electron sum app with CI and two-input UI

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+﻿import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+function createWindow(): void {
+  const win = new BrowserWindow({
+    width: 420,
+    height: 300,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  win.removeMenu();
+  win.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ src/preload.ts
@@ -0,0 +1,7 @@
+﻿import { contextBridge } from "electron";
+
+contextBridge.exposeInMainWorld("api", {
+  sum(numbers: number[]): number {
+    return numbers.reduce((acc, n) => acc + n, 0);
+  },
+});
